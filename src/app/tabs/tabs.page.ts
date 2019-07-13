fdad8f349aa9a048d5bdc8c704eeabe957366b1c
joh95 <johan.ospina@hotmail.com>
2019-07-13 09:30:35 -0500

    Dojo Ionic

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Camera} from '@ionic-native/camera/ngx';
+
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +9,35 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  foto:any;
 
+  constructor(private camera: Camera) {}
+
+  capturarFoto(){
+    this.camera.getPicture({
+      quality:100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.CAMERA
+    }).then(dataImage =>{
+      this.foto = 'data:image/jpeg;base64,'+dataImage;
+    })
+  }
+
+  buscarFoto(){
+    this.camera.getPicture({
+      quality:100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      allowEdit: false,
+      saveToPhotoAlbum: true,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }).then(dataImage =>{
+      this.foto = 'data:image/jpeg;base64,'+dataImage;
+    })
+  }
 }
